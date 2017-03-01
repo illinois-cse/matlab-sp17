a446b499dec27e3a9063408a61f6b8ebb8f0c1ad
jjlee1 <jjlee1@illinois.edu>
2017-02-28 21:48:13 -0600

    Delete lorenz.m

--- lorenz.m
+++
@@ -1,9 +0,0 @@
-function xprime = lorenz(t,x);
-%LORENZ: Computes the derivatives involved in solving the
-%Lorenz equations
-% for the purposes of this example, we will take ? = 10, ? = 8/3, and ? = 28, as well as
-%x(0) = ?8, y(0) = 8, and z(0) = 27.
-sig=10;
-beta=8/3;
-rho=28;
-xprime=[-sig*x(1) + sig*x(2); rho*x(1) - x(2) - x(1)*x(3); -beta*x(3) + x(1)*x(2)];
